a01057c869d9010c811d5d7b9d1792cc7eb1d869
Peter Esselius <peter@saltside.se>
2016-08-15 13:21:13 +0200

    Updates

--- link/config/fish/config.fish
+++ link/config/fish/config.fish
@@ -2,12 +2,6 @@ set fish_greeting
 
 set -x EDITOR vim
 
-setup-wrk
-
-set PATH $HOME/.rbenv/bin $PATH
-set PATH $HOME/.rbenv/shims $PATH
-rbenv rehash >/dev/null ^&1
-
 function __prompt_context_reload -v __prompt_context_current
   echo $__prompt_context_current
 end
