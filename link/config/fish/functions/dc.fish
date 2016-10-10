2ed9c78fa548bef3890654ae71e2058739d3f7ee
Peter Esselius <peter@saltside.se>
2016-10-10 21:07:37 +0200

    Meh

--- link/config/fish/functions/dc.fish
+++ link/config/fish/functions/dc.fish
@@ -1,3 +1,8 @@
 function dc
-  docker rmi (docker images | grep '<none>' | awk '{print $3}')
+  echo "Removing containers"
+  docker rm -f (docker ps -qa) 2>/dev/null
+  echo "Removing untagged images"
+  docker rmi (docker images | grep '<none>' | awk '{print $3}') 2>/dev/null
+  echo "Removing networks"
+  docker network rm (docker network ls | grep -Ev 'bridge\s+bridge|host|null|NETWORK' | awk '{print $1}') 2>/dev/null
 end
--- link/config/fish/functions/gp.fish
+++ link/config/fish/functions/gp.fish
@@ -1,4 +1,4 @@
 function gp
   set BRANCH (git rev-parse --abbrev-ref HEAD)
-  git push --set-upstream origin $BRANCH 
+  git push --set-upstream origin $BRANCH $argv
 end
