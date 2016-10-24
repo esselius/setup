738c08adcee07a9f48539c04e8978068adb14991
Peter Esselius <peter@saltside.se>
2016-10-24 09:29:50 +0200

    Make "gp" compatible with new branches

--- link/config/fish/functions/gp.fish
+++ link/config/fish/functions/gp.fish
@@ -1,4 +1,8 @@
 function gp
   set BRANCH (git rev-parse --abbrev-ref HEAD)
-  git push --force-with-lease --set-upstream origin $BRANCH $argv
+  if git show-branch origin/$BRANCH > /dev/null 2>&1
+    git push --force-with-lease --set-upstream origin $BRANCH $argv
+  else
+    git push --set-upstream origin $BRANCH $argv
+  end
 end
