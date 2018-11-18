c7dbc04cfc4de578c828d4015fcb3af8bbcb52a1
Peter Esselius <esselius@users.noreply.github.com>
2018-11-18 12:45:17 +0100

    Delete setup-wrk.fish

--- link/config/fish/functions/setup-wrk.fish
+++
@@ -1,7 +0,0 @@
-function setup-wrk
-  set -x SALTSIDE_SOURCE_DIR ~/code
-  set -x WORKSTATION_VAGRANTFILE $SALTSIDE_SOURCE_DIR/platform-workstation/Vagrantfile
-  set PATH $SALTSIDE_SOURCE_DIR/platform-workstation/vagrant-workstation/bin/ $PATH
-  
-  alias wrk workstation
-end
