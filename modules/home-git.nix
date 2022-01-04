2e1b091fd01a59d4b7589d895e3354f124de6d59
Peter Esselius <peter.esselius@pagero.com>
2022-01-04 10:49:06 +0100

    git: github repo sync scripts

--- modules/home-git.nix
+++ modules/home-git.nix
@@ -1,66 +1,92 @@
+{ pkgs, ... }:
 {
-  programs.ssh = {
-    enable = true;
-    controlMaster = "auto";
-    controlPersist = "60m";
-    controlPath = "/tmp/.ssh-%C";
-  };
-
-  programs.git = {
-    enable = true;
-    lfs.enable = true;
-
-    userName = "Peter Esselius";
-
-    delta = {
+  programs = {
+    ssh = {
       enable = true;
-      options = {
-        # features = "side-by-side line-numbers decorations";
-        syntax-theme = "Solarized (light)";
-      };
+      controlMaster = "auto";
+      controlPersist = "60m";
+      controlPath = "/tmp/.ssh-%C";
     };
 
-    aliases = {
-      l = "log --graph --decorate --pretty=format:\"%C(yellow)%h%C(reset)%C(auto)%d%C(reset) %s %C(yellow)(%C(cyan)%ar%C(yellow), %C(blue)%an%C(yellow))%C(reset)\"";
-      ll = "log --graph --decorate --stat --pretty=format:\"%C(yellow)%h%C(reset)%C(auto)%d%C(reset) %s%n %C(cyan)%ar%C(reset), %C(blue)%an%C(reset)%n\"";
-      wc = "whatchanged -p --abbrev-commit --pretty=medium";
-      dc = "diff --cached";
-      unstage = "reset HEAD";
-      uncommit = "reset --soft HEAD^";
-    };
+    git = {
+      enable = true;
+      lfs.enable = true;
 
-    ignores = [
-      ".*.swp"
-      ".DS_Store"
-      ".metals/"
-      ".bloop/"
-      ".vscode/"
-    ];
+      userName = "Peter Esselius";
+      userEmail = "peter.esselius@pagero.com";
 
-    extraConfig = {
-      init = {
-        defaultBranch = "main";
+      delta = {
+        enable = true;
+        options = {
+          # features = "side-by-side line-numbers decorations";
+          syntax-theme = "Solarized (light)";
+        };
       };
-      rebase = {
-        autoSquash = true;
-        autoStash = true;
+
+      aliases = {
+        l = "log --graph --decorate --pretty=format:\"%C(yellow)%h%C(reset)%C(auto)%d%C(reset) %s %C(yellow)(%C(cyan)%ar%C(yellow), %C(blue)%an%C(yellow))%C(reset)\"";
+        ll = "log --graph --decorate --stat --pretty=format:\"%C(yellow)%h%C(reset)%C(auto)%d%C(reset) %s%n %C(cyan)%ar%C(reset), %C(blue)%an%C(reset)%n\"";
+        wc = "whatchanged -p --abbrev-commit --pretty=medium";
+        dc = "diff --cached";
+        unstage = "reset HEAD";
+        uncommit = "reset --soft HEAD^";
       };
-      rerere = {
-        enabled = true;
-        autoUpdate = true;
-      };
-      pull = {
-        rebase = true;
-      };
-      push = {
-        default = "simple";
-      };
-      color = {
-        ui = "auto";
-      };
-      ghq = {
-        root = "~/src";
+
+      ignores = [
+        ".*.swp"
+        ".DS_Store"
+        ".metals/"
+        ".bloop/"
+        ".vscode/"
+      ];
+
+      extraConfig = {
+        init = {
+          defaultBranch = "main";
+        };
+        rebase = {
+          autoSquash = true;
+          autoStash = true;
+        };
+        rerere = {
+          enabled = true;
+          autoUpdate = true;
+        };
+        pull = {
+          rebase = true;
+        };
+        push = {
+          default = "simple";
+        };
+        color = {
+          ui = "auto";
+        };
+        ghq = {
+          root = "~/src";
+        };
+        url."git@github.com:".insteadOf = "https://github.com";
       };
     };
   };
+
+  home.packages = [
+    pkgs.gh
+
+    (pkgs.writers.writeBashBin "github-repos" ''
+      ${pkgs.gh}/bin/gh repo list "$1" -L 1000 --json sshUrl -q '.[].sshUrl'
+    '')
+
+    (pkgs.writers.writeBashBin "github-starred" ''
+      ${pkgs.gh}/bin/gh api /user/starred --paginate -q '.[].ssh_url'
+    '')
+
+    (pkgs.writers.writeBashBin "github-sync" ''
+      github-starred | ${pkgs.ghq}/bin/ghq get "$@"
+      github-repos "$GITHUB_USER" | ${pkgs.ghq}/bin/ghq get "$@"
+
+      for org in $GITHUB_ORGS; do
+        github-repos "$org" | ${pkgs.ghq}/bin/ghq get "$@"
+      done
+    '')
+  ];
 }
